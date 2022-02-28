c6d99bb7b61b4c3e43bb975ff53edc31bfa0265b
Ibrahim Alobaysi <Ibrahim.Alobaysi@tuwaiq.edu.sa>
2022-02-28 21:12:05 +0300

    solving CORS

--- src/main/java/com/example/springbootsecuritydbdemo/security/AppSecurityConfig.java
+++ src/main/java/com/example/springbootsecuritydbdemo/security/AppSecurityConfig.java
@@ -36,8 +36,10 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .cors()
+                .and()
                 .authorizeRequests()
-                .antMatchers("/admin/management").hasAnyAuthority("ADMIN:READ")
+                .antMatchers("/admin/management").hasRole("ADMIN")
                 .antMatchers("**/user/**").hasRole("USER")
                 .anyRequest().permitAll()
 //                .authenticated()
