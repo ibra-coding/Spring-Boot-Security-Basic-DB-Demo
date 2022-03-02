934ecca39ba781596a1a70a4fd6dff8b70ea0c63
Ibrahim Alobaysi <Ibrahim.Alobaysi@tuwaiq.edu.sa>
2022-03-02 14:28:18 +0300

    adding few things like exception and others

--- src/main/java/com/example/springbootsecuritydbdemo/controllers/Management.java
+++ src/main/java/com/example/springbootsecuritydbdemo/controllers/Management.java
@@ -1,26 +1,33 @@
 package com.example.springbootsecuritydbdemo.controllers;
 
+import com.example.springbootsecuritydbdemo.models.MyUser;
 import com.example.springbootsecuritydbdemo.security.UserPrincipalService;
+import com.example.springbootsecuritydbdemo.services.MyUserService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("admin/management")
+@RequestMapping("api/v1/admin/management")
 public class Management {
 
-    private final UserPrincipalService userPrincipalService;
+    private final MyUserService myUserService;
 
-    public Management(UserPrincipalService userPrincipalService) {
-        this.userPrincipalService = userPrincipalService;
+    public Management(MyUserService myUserService) {
+        this.myUserService = myUserService;
     }
 
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public String auth() {
-        return "YOU ARE OK";
+    public List<MyUser> getAllUsers() {
+        return myUserService.getAllUser();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public MyUser getUser(@PathVariable Long id) {
+        return myUserService.getUser(id);
     }
 }
